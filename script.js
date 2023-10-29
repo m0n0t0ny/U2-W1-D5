270e8aa81bcfd932d434b2ff10885ea524f672ea
Antonio Bertuccio <anto.bertu@gmail.com>
2023-10-29 18:46:46 +0100

    features and fix add
    - add new fonts (the real onews from Medium website 😇).
    - fixed header color shift. Color now shifts after 300px.
    - add beta JS of hero Ms animation (not fully working yet 😤)
    - on smaller devices nav links now disappear (except for the "Sign In" link)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const getStarted = document.querySelector("#get-started");
 
   window.addEventListener("scroll", function () {
-    if (window.scrollY > header.clientHeight) {
+    if (window.scrollY > 300) {
       header.classList.add("white-bg");
       getStarted.classList.add("green-bg");
     } else {
@@ -12,3 +12,50 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+/* TODO header M should appear and disappear like in the original website */
+
+// svg M spawner/despawner
+
+// function toggleOpacityInfinite() {
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const allM = document.querySelectorAll("g");
+
+//     allM.forEach((M) => {
+//       const appearTime = Math.floor(Math.random() * 5000); // Rounded to the nearest integer
+//       const disappearTime = Math.floor(Math.random() * 5000); // Rounded to the nearest integer
+
+//       M.style.opacity = Math.random() < 0.5 ? 0 : 1;
+
+//       setTimeout(makeItAppear, appearTime);
+//       setTimeout(makeItDisappear, disappearTime);
+
+//       function makeItAppear() {
+//         M.style.opacity = 1;
+//       }
+
+//       function makeItDisappear() {
+//         M.style.opacity = 0;
+//       }
+//     });
+
+//     setTimeout(toggleOpacityInfinite, 5000);
+//   });
+// }
+
+// toggleOpacityInfinite();
+
+const allM = document.querySelectorAll("g");
+
+function toggleM() {
+  allM.forEach((M) => {
+    const randomTime = Math.round(Math.random() * 30000);
+    setTimeout(function () {
+      const toggleOpacity = Math.round(Math.random() * 2);
+      M.style.opacity = toggleOpacity;
+    }, randomTime);
+  });
+
+  setInterval(toggleM, 60000);
+}
+toggleM();
